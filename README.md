77c6bb703a25948276430b4f0c99793f26c6a962
haz3141 <38766403+haz3141@users.noreply.github.com>
2022-08-12 14:08:12 -0400

    Setup hardhat.config.js

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Full Stack NFT marketplace built using Next.js, IPFS, Solidity, and Polygon
 
 ```
-Source: Nader Dabit - Includes Article and Code
+Source: Nader Dabit
 Article: https://dev.to/edge-and-node/building-scalable-full-stack-apps-on-ethereum-with-polygon-2cfb
 Code: https://github.com/dabit3/polygon-ethereum-nextjs-marketplace/
 ```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 4774
+    },
+    //mumbai: {
+    //  url: "https://rpc-mumbai.maticvigil.com",
+    //  accounts: [process.env.privateKey]
+    //}
+  },
   solidity: "0.8.9",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
 };
